6b74b8480d357d874188b7b78b89c63e84b9de9e
Garrett Griffith <g.griffith@cpanel.net>
2016-09-01 09:05:07 -0500

    Adding the first three chapters' exercises (Chapter 3 is empty right now) Please enter the commit message for your changes. Lines starting

---
+++ CH1/Helloword.pm
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+print "\n
+\n
+Hello, world\n
+Garrett is the man!\n
+\n
+\n";
+print "The answer is ";
+print 6 * 7;
+print ".\n";
+
+print "The other answer is ", 23 * 3, ".\n";
---
+++ CH2/circ2.pm
@@ -0,0 +1,6 @@
+# Exercise 2.2 - This program calculates the circumference of a cirlce with a radius of 12.5
+print "Enter the radius: ";
+$rad = <STDIN>;
+$circ = 2 * 3.141592654 * $rad;
+print "The circumference is $circ.\n";
+
